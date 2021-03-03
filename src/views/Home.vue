602bcb4d21421268a074ebfc4b77de803df772ab
Alexander Todorov <aleksandar9999a@gmail.com>
2021-03-03 12:58:50 +0200

    Init

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    template
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({})
+export default class Home extends Vue {}
+</script>
